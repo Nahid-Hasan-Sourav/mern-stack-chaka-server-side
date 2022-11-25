a0ba31e00ce8a80e8f2595acfccbf554b8c51109
Nahid-Hasan_Sourav <nahidhasansourav05@gmail.com>
2022-11-25 22:56:40 +0600

    successfullt create api for getting data from mongo db and this is my first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sfv0aqa.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// console.log(uri)
+
+
+async function run() {
+        try{
+            const categoryCollection=client.db('chakadb').collection('categories');
+            const allCategoriesItemsCollection=client.db('chakadb').collection('allCategoriesItems');
+
+            //GET THE ALL CATEGORIES
+            app.get('/categories', async(req, res) =>{           
+              const query ={}
+              const categories = await  categoryCollection.find(query).toArray();
+              console.log(categories);
+              res.send(categories);
+            })
+
+            // GET THE SPECIFIC CATEGORY BY CATEGORY NAME
+            app.get('/categorie/:id', async(req, res)=>{
+              const categoryNames =req.params.id;
+              console.log("Category Name ",categoryNames);
+              
+              const query={
+                categoryName:categoryNames
+              }
+              console.log("query",query);
+              // const cursor= allCategoriesItemsCollection.find(query);
+              const items= await allCategoriesItemsCollection.find(query).toArray();
+              console.log("Services",items);
+              res.send(items);
+              
+             })
+
+
+        
+        }
+        finally{
+
+        }
+
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello From Chaka!')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Chaka App listening on port ${port}`)
+  })
